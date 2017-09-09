b0aec93aa9bdc0c4509dc8d678ae9495a5bd7c6f
Hatsumi Hashi <hatsumihashi@mbp-de-hatsumi.home>
2017-09-08 22:12:57 -0300

    Primeiro commit

---
+++ CStudies/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  CStudies
+//
+//  Created by Hatsumi Hashi on 05/09/17.
+//  Copyright © 2017 Hatsumi Hashi. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    printf( "I am alive!  Beware.\n" );
+    getchar();
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Studies of C Language
