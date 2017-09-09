2d048889a464fa76960da45b0ef221105051518e
Hatsumi Hashi <hatsumihashi@mbp-de-hatsumi.home>
2017-09-08 22:39:28 -0300

    Adicionando método que utiliza condicionais 'if' e 'else'

--- CStudies/main.c
+++ CStudies/main.c
@@ -9,11 +9,12 @@
 #include <stdio.h>
 
 void Saida();
-void Entrada();
+int Entrada();
+void Condicionais();
 
 int main()
 {
-    Entrada();
+    Condicionais();
 }
 
 void Saida(){
@@ -21,12 +22,26 @@ void Saida(){
     getchar();
 }
 
-void Entrada(){
+int Entrada(){
     int numero;
     
     printf( "Insira um número: " );
     scanf( "%d", &numero );
-    printf( "Você inseriu o número %d\n", numero );
+    printf( "Você inseriu o número %d.\n", numero );
     getchar();
+    
+    return numero;
 }
 
+void Condicionais(){
+    int numero = Entrada();
+
+    
+    if ( numero == 1 ) {
+        printf("Executa esse comando se a condição do if for verdadeira.\n");
+        printf("A condição do if vai ser verdadeira se o número for igual a 1.\n");
+    }
+    else {
+        printf("Executa esse comando se a condição do if for falsa.\n");
+    }
+}
