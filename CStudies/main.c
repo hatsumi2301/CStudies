df64fe8b986c8ce7199f303635ebf0e47a328fea
Hatsumi Hashi <hatsumihashi@mbp-de-hatsumi.home>
2017-09-08 22:42:58 -0300

    Alterando método de condicionais para utilizar 'else if'

--- CStudies/main.c
+++ CStudies/main.c
@@ -36,12 +36,15 @@ int Entrada(){
 void Condicionais(){
     int numero = Entrada();
 
-    
     if ( numero == 1 ) {
-        printf("Executa esse comando se a condição do if for verdadeira.\n");
+        printf("Executa esse comando se a condição do primeiro if for verdadeira.\n");
         printf("A condição do if vai ser verdadeira se o número for igual a 1.\n");
     }
+    else if(numero == 2){
+        printf("Executa esse comando se a condição do primeiro if for falsa e a condição do segundo if for verdadeira.\n");
+        printf("A condição do if vai ser verdadeira se o número for igual a 2.\n");
+    }
     else {
-        printf("Executa esse comando se a condição do if for falsa.\n");
+        printf("Executa esse comando se a condição dos ifs forem falsas.\n");
     }
 }
