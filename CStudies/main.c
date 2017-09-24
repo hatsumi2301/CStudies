d49ef1c1659c67ba3e7084bffc8b1391f6e6c96d
Hatsumi Hashi <hatsumih@hotmail.com>
2017-09-23 23:59:50 -0300

    Organizando o código.

--- CStudies/main.c
+++ CStudies/main.c
@@ -12,44 +12,12 @@
 #include "Loop.h"
 
 int x, y; //Variáveis globais
-void FuncaoQueUtilizaAsVariaveisGlobais(void);
-void FuncaoQueUtilizaAsVariaveisGlobais2(void);
-void FuncaoQueUtilizaAsVariaveisGlobais3(void);
-
-void Condicionais(void); //Exemplo de condicionais
-
-void LoopFor(void); //Exemplo de loop for
-void LoopWhile(void); //Exemplo de loop while
-void LoopDoWhile(void); //Exemplo de loop do while
-void DiferencaEntreLoopWhileAndDoWhile(void); //Exemplifica a diferente entre o loop do tipo while e o do do while
-
-void TiposDeVariaveis(void); //Imprime um exemplo dos tipos de variáveis
-void ModificadoresDeVariaveis(void); //Imprime a lista dos tipos e modificadores de variáveis
-void VariavelDoTipoConst(const char *palavra); //Imprime '-' no lugar dos espaços no texto passado como parâmetro
-void VariavelDoTipoVolatile(void); //Imprime um texto explicando o que é uma variável do tipo volatile
-void EspecificadorExtern(void); //Imprime um texto explicando o que é extern
-void EspecificadorStatic(void); //Imprime um texto explicando o que é o especificador static
-void VariavelEstaticaGlobal(void); //Imprime um texto explicando sobre o contexto da variável estática global
-void IniciarSerie(int semente); //Inicializa a variável estática global da classe VariavelEstatica
-int Serie(void); //Soma um valor à variável estática global
-int EspecificadorRegister(register int m, register int e); //Imprime um texto explicando
-
-void AtribuicoesMultiplas(void); //Exemplo de múltiplas atribuições
-
-void ObterNome(void);
-
-void Constantes(void); //Imprime uma tabela com as constantes \
-
-void Modulo(void); //Exemplo de modulo
-
-void IncrementoDecremento(void); //Exemplo de incrementos e decrementos
-void PrintXor(void); //Exemplo de xor
-void OperadoresBitBit(void); //Imprime tabela de operadores bit a bit
 
 int main(){
     while (1==1) {
         int opcao = Opcoes();
         int opcao2;
+        int existe;
     
         switch (opcao) {
             case 1:
@@ -67,7 +35,6 @@ int main(){
                 
                 break;
             case 3:
-                int existe;
                 printf("\nInforme uma opção: \n");
                 printf("1. Verificar se o caractere existe na palavra informando a palavra e o caractere \n2. Verificar que o caractere 'x' não existe na palavra 'fécula'\n3. Verificar que o caractere 'a' existe na palavra 'ave'\n\n");
                 scanf("%d", &opcao2);
